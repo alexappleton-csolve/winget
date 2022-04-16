837679720c65ff501811029b39a088a5db126f1d
Alex Appleton <47610087+alexappleton-csolve@users.noreply.github.com>
2022-04-16 10:23:05 -0400

    Update winget.psm1

--- winget.psm1
+++ winget.psm1
@@ -130,7 +130,7 @@ Function Get-WGList {
     }
 
     #Remove junk characters from results
-    $listresult = $listresult -replace "ΓÇª", " "
+    $listresult = $listresult -replace "[ΓÇª]", " "
 
     #Split winget output to lines
     $lines = $listResult.Split([Environment]::NewLine).Replace("¦ ","")
