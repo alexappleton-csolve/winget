5ee4e01c8855f4cd138741334b49774bc4f19fb0
Alex Appleton <47610087+alexappleton-csolve@users.noreply.github.com>
2022-12-08 16:20:59 -0500

    Streamline get-wgupgrade to use parse function

--- winget.psm1
+++ winget.psm1
@@ -204,59 +204,16 @@ Function Get-WGList2 {
 }
 
 #following function lists only the apps that require updating
-function Get-WGUpgrade {
-	class Software {
-        [string]$Name
-        [string]$Id
-        [string]$Version
-        [string]$AvailableVersion
+Function Get-WGUpgrade {
+    if((Test-WG)){
+        $results = winget upgrade
+        $upgradeableApps = Parse-WingetListOutput $results
+        return $upgradeableApps
     }
-
-    #Get list of available upgrades on winget format
-    $upgradeResult = & $Winget upgrade | Out-String
-
-    #Start Conversion of winget format to an array. Check if "-----" exists
-    if (!($upgradeResult -match "-----")){
-        return
+    else {
+        Write-Log "Winget not installed" -Severity 'Error'
+        return $false
     }
-
-    #Split winget output to lines
-    $lines = $upgradeResult.Split([Environment]::NewLine).Replace("¦ ","")
-
-    # Find the line that starts with "------"
-    $fl = 0
-    while (-not $lines[$fl].StartsWith("-----")){
-        $fl++
-    }
-    
-    #Get header line
-    $fl = $fl - 2
-
-    #Get header titles
-    $index = $lines[$fl] -split '\s+'
-
-    # Line $i has the header, we can find char where we find ID and Version
-    $idStart = $lines[$fl].IndexOf($index[1])
-    $versionStart = $lines[$fl].IndexOf($index[2])
-    $availableStart = $lines[$fl].IndexOf($index[3])
-    $sourceStart = $lines[$fl].IndexOf($index[4])
-
-    # Now cycle in real package and split accordingly
-    $upgradeList = @()
-    For ($i = $fl + 2; $i -le $lines.Length; $i++){
-        $line = $lines[$i]
-        if ($line.Length -gt ($sourceStart+5) -and -not $line.StartsWith('-')){
-            $software = [Software]::new()
-            $software.Name = $line.Substring(0, $idStart).TrimEnd()
-            $software.Id = $line.Substring($idStart, $versionStart - $idStart).TrimEnd()
-            $software.Version = $line.Substring($versionStart, $availableStart - $versionStart).TrimEnd()
-            $software.AvailableVersion = $line.Substring($availableStart, $sourceStart - $availableStart).TrimEnd()
-            #add formated soft to list
-            $upgradeList += $software
-        }
-    }
-
-    return $upgradeList
 }
 
 #following function parses the results of winget
