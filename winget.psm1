c47f9a8d76faeaeadf8dd242a661b8f7ee06cf15
Alex Appleton <alex.appleton@csolve.ca>
2022-12-19 21:27:53 -0500

    Parsing debug

--- winget.psm1
+++ winget.psm1
@@ -233,7 +233,7 @@ Function Process-WingetResults {
     # Filter the output to select only the lines that match certain criteria
     $filteredResults = $normalizedResults | Where-Object {
         # Use a regular expression to match lines that contain words with basic Latin characters - still needs work
-         $_ -match '[A-Za-z].*[A-Za-z]' -and -not ($_ -match '^\s*$')
+         $_ -match '[A-Za-z].*[A-Za-z]' -and $_ -notmatch '\x{0000}-\x{001F}'
     }
 
     $filteredResults = [regex]::Replace($filteredResults, "[^\p{IsBasicLatin}]", "")
